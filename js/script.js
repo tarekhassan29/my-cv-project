a061b157a62d2b3d30deb258f4b1e7e0f1c96fc6
Tarek Hassan <tareknetworks@gmail.com>
2025-08-18 15:28:38 +0300

    Add some javascript code

---
+++ js/script.js
@@ -0,0 +1,4 @@
+const aboutMe = document.getElementById('about-me');
+aboutMe.addEventListener("click",function(){
+    aboutMe.textContent("Thanks for clicking! I’m excited to grow as a developer!");
+});
